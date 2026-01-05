9aa2bfb30b93c57881ddc82a1a7adcd500f193b0
Guilherme <guilhermerochas0420@gmail.com>
2026-01-05 20:35:19 -0300

    feat: add rastreamento page

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RastreamentoController;
 
 Route::get('/', HomeController::class);
+Route::get('/rastreamento', RastreamentoController::class);
