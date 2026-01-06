1df9563b9d7be1a6bfab359b581ceb7804d44619
Guilherme <guilhermerochas0420@gmail.com>
2026-01-05 21:54:27 -0300

    feat: added Historico page, routes and controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RastreamentoController;
+use App\Http\Controllers\HistoricoController;
 
 Route::get('/', HomeController::class);
 Route::get('/rastreamento', RastreamentoController::class)->name('frete.rastreamento');
+Route::get('/historico', HistoricoController::class)->name('frete.historico');
