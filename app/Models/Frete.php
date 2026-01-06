2f28784a3add0bbf947c8c36767d0bb4ce9579d7
Guilherme <guilhermerochas0420@gmail.com>
2026-01-05 22:24:21 -0300

    feat: added fretestatus enums

--- app/Models/Frete.php
+++ app/Models/Frete.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Enums\FreteStatus;
 
 class Frete extends Model
 {
+    protected $casts = [
+        'status' => FreteStatus::class,
+    ];
+
     public function etapas(): HasMany
     {
         return $this->hasMany(Etapa::class);
