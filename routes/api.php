25f9aeb3b3824004818737351790e3f0bd388611
Guilherme <guilhermerochas0420@gmail.com>
2026-01-06 21:36:41 -0300

    feat: added post methos for the api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ola', function () {
     return 'Ola Mundo!';
 });
+
+Route::post('/clientes', [ClienteController::class, 'store']);
